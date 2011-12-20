e863bfe61ce6b6400f939c96425784dfbd857915
Lukasz Rozycki <lukasz.rozycki.agh@gmail.com>
2011-12-20 21:44:17 +0100

    minor changes

--- runSimulation.sh
+++ runSimulation.sh
@@ -2,16 +2,35 @@
 
 JAVA=`cygpath 'C:\Program Files\Java\jdk1.7.0_01\bin\java'`
 
-servers=( SS SJ LS LJ CS CJ IM )
+servers=( XX )
 
 #SS SJ LS LJ CS CJ IM AS AJ
 
-users=( 1 2 5 10 20 )
+users=( 10 20 )
 writers=( 0 1000 100 10 )
 
 for server in ${servers[@]}
 do
 	for user in ${users[@]}
+	do
+		for writer in ${writers[@]}
+		do
+			command=`echo -server \
+				-Dfile.encoding=UTF-8 \
+				-jar target/simulation.jar "$@" -f -u $user -w $writer $server`
+			echo '******************************************************************************'
+			echo "$JAVA" $command
+			"$JAVA" $command
+			echo '------------------------------------------------------------------------------'
+		done
+	done
+done
+
+actors=( AS AJ )
+
+for server in ${servers[@]}
+do
+	for user in ${actors[@]}
 	do
 		for writer in ${writers[@]}
 		do
@@ -25,4 +44,3 @@ do
 		done
 	done
 done
-
