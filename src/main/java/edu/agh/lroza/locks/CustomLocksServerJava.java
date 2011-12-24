0142f8af20c1fcdf595311a1c987a58983fa6f22
Lukasz Rozycki <lukasz.rozycki.agh@gmail.com>
2011-12-24 12:16:25 +0100

    migrated CustomLocksServerJava from Scala to Java interface

--- src/main/java/edu/agh/lroza/locks/CustomLocksServerJava.java
+++ src/main/java/edu/agh/lroza/locks/CustomLocksServerJava.java
@@ -1,199 +1,184 @@
-//package edu.agh.lroza.locks;
-//
-//import static edu.agh.lroza.javacommon.UtilsJ.left;
-//import static edu.agh.lroza.javacommon.UtilsJ.newProblem;
-//import static edu.agh.lroza.javacommon.UtilsJ.none;
-//import static edu.agh.lroza.javacommon.UtilsJ.right;
-//import static edu.agh.lroza.javacommon.UtilsJ.some;
-//
-//import java.util.HashMap;
-//import java.util.HashSet;
-//import java.util.UUID;
-//import java.util.concurrent.locks.ReadWriteLock;
-//import java.util.concurrent.locks.ReentrantReadWriteLock;
-//
-//import edu.agh.lroza.common.Id;
-//import edu.agh.lroza.common.Notice;
-//import edu.agh.lroza.common.NoticeBoardServerScala;
-//import edu.agh.lroza.javacommon.NoticeJ;
-//import edu.agh.lroza.common.Problem;
-//import edu.agh.lroza.javacommon.UtilsJ;
-//import scala.Either;
-//import scala.Option;
-//import scala.collection.JavaConversions;
-//import scala.collection.Set;
-//
-//import com.google.common.collect.ImmutableSet;
-//
-//public class CustomLocksServerJava implements NoticeBoardServerScala {
-//    private java.util.Set<UUID> loggedUsers = new HashSet<>();
-//    private java.util.Map<Id, Notice> notices = new HashMap<>();
-//
-//    private ReadWriteLock loggedUsersLock = new ReentrantReadWriteLock();
-//    private ReadWriteLock noticesLock = new ReentrantReadWriteLock();
-//
-//    private static class TitleId implements Id {
-//        private final String title;
-//
-//        public TitleId(String title) {
-//            this.title = title;
-//        }
-//
-//        @Override
-//        public boolean equals(Object obj) {
-//            if (obj instanceof TitleId) {
-//                return ((TitleId) obj).title.equals(title);
-//            } else {
-//                return false;
-//            }
-//        }
-//
-//        @Override
-//        public int hashCode() {
-//            return title.hashCode();
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "TitleId(" + title + ")";
-//        }
-//    }
-//
-//    private boolean isValid(UUID token) {
-//        loggedUsersLock.readLock().lock();
-//        try {
-//            return loggedUsers.contains(token);
-//        } finally {
-//            loggedUsersLock.readLock().unlock();
-//        }
-//    }
-//
-//    public Either<Problem, UUID> login(String username, String password) {
-//        if (username.equals(password)) {
-//            UUID token = UUID.randomUUID();
-//            loggedUsersLock.writeLock().lock();
-//            try {
-//                loggedUsers.add(token);
-//            } finally {
-//                loggedUsersLock.writeLock().unlock();
-//            }
-//            return UtilsJ.right(token);
-//        } else {
-//            return left(newProblem("Wrong password"));
-//        }
-//    }
-//
-//    public Option<Problem> logout(UUID token) {
-//        loggedUsersLock.writeLock().lock();
-//        try {
-//            if (loggedUsers.remove(token)) {
-//                return Option.empty();
-//            } else {
-//                return UtilsJ.some(newProblem("Invalid token"));
-//            }
-//        } finally {
-//            loggedUsersLock.writeLock().unlock();
-//        }
-//    }
-//
-//    public Either<Problem, Set<Id>> listNoticesIds(UUID token) {
-//        if (isValid(token)) {
-//            noticesLock.readLock().lock();
-//            try {
-//                return UtilsJ.right((Set<Id>) JavaConversions.asScalaSet(ImmutableSet.copyOf(notices.keySet())));
-//            } finally {
-//                noticesLock.readLock().unlock();
-//            }
-//        } else {
-//            return left(newProblem("Invalid token"));
-//        }
-//    }
-//
-//    @Override
-//    public Either<Problem, Id> addNotice(UUID token, String title, String message) {
-//        if (isValid(token)) {
-//            Id id = new TitleId(title);
-//            noticesLock.writeLock().lock();
-//            try {
-//                if (notices.get(id) == null) {
-//                    notices.put(id, new NoticeJ(title, message));
-//                    return UtilsJ.right(id);
-//                } else {
-//                    return left(newProblem("Topic with title '" + title + "' already exists"));
-//                }
-//            } finally {
-//                noticesLock.writeLock().unlock();
-//            }
-//        } else {
-//            return left(newProblem("Invalid token"));
-//        }
-//    }
-//
-//    @Override
-//    public Either<Problem, Notice> getNotice(UUID token, Id id) {
-//        if (isValid(token)) {
-//            Notice notice;
-//            noticesLock.readLock().lock();
-//            try {
-//                notice = notices.get(id);
-//            } finally {
-//                noticesLock.readLock().unlock();
-//            }
-//            if (notice == null) {
-//                return left(newProblem("There is no such notice '" + id + "'"));
-//            } else {
-//                return UtilsJ.right(notice);
-//            }
-//        } else {
-//            return left(newProblem("Invalid token"));
-//        }
-//    }
-//
-//    @Override
-//    public Either<Problem, Id> updateNotice(UUID token, Id id, String title, String message) {
-//        if (isValid(token)) {
-//            noticesLock.writeLock().lock();
-//            try {
-//                final Notice notice = notices.get(id);
-//                if (notice == null) {
-//                    return left(newProblem("There is no such notice '" + id + "'"));
-//                } else {
-//                    Id newId = new TitleId(title);
-//                    if (!newId.equals(id) && notices.containsKey(newId)) {
-//                        return left(newProblem("Topic with title '" + title + "' already exists"));
-//                    } else {
-//                        if (!newId.equals(id)) {
-//                            notices.remove(id);
-//                        }
-//                        notices.put(newId, new NoticeJ(title, message));
-//                        return right(newId);
-//                    }
-//                }
-//            } finally {
-//                noticesLock.writeLock().unlock();
-//            }
-//        } else {
-//            return left(newProblem("Invalid token"));
-//        }
-//    }
-//
-//    @Override
-//    public Option<Problem> deleteNotice(UUID token, Id id) {
-//        if (isValid(token)) {
-//            Notice notice;
-//            noticesLock.writeLock().lock();
-//            try {
-//                notice = notices.remove(id);
-//            } finally {
-//                noticesLock.writeLock().unlock();
-//            }
-//            if (notice == null) {
-//                return some(newProblem("There is no such notice '" + id + "'"));
-//            } else {
-//                return none();
-//            }
-//        } else {
-//            return some(newProblem("Invalid token"));
-//        }
-//    }
-//}
+package edu.agh.lroza.locks;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import edu.agh.lroza.common.Id;
+import edu.agh.lroza.javacommon.Notice;
+import edu.agh.lroza.javacommon.NoticeBoardServerJava;
+import edu.agh.lroza.javacommon.ProblemException;
+
+import com.google.common.collect.ImmutableSet;
+
+public class CustomLocksServerJava implements NoticeBoardServerJava {
+    private java.util.Set<UUID> loggedUsers = new HashSet<>();
+    private java.util.Map<Id, Notice> notices = new HashMap<>();
+
+    private ReadWriteLock loggedUsersLock = new ReentrantReadWriteLock();
+    private ReadWriteLock noticesLock = new ReentrantReadWriteLock();
+
+    private static class TitleId implements Id {
+        private final String title;
+
+        public TitleId(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof TitleId) {
+                return ((TitleId) obj).title.equals(title);
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return title.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "TitleId(" + title + ")";
+        }
+    }
+
+    private boolean isValid(UUID token) {
+        loggedUsersLock.readLock().lock();
+        try {
+            return loggedUsers.contains(token);
+        } finally {
+            loggedUsersLock.readLock().unlock();
+        }
+    }
+
+    public UUID login(String username, String password) throws ProblemException {
+        if (username.equals(password)) {
+            UUID token = UUID.randomUUID();
+            loggedUsersLock.writeLock().lock();
+            try {
+                loggedUsers.add(token);
+            } finally {
+                loggedUsersLock.writeLock().unlock();
+            }
+            return token;
+        } else {
+            throw new ProblemException("Wrong password");
+        }
+    }
+
+    public void logout(UUID token) throws ProblemException {
+        loggedUsersLock.writeLock().lock();
+        try {
+            if (!loggedUsers.remove(token)) {
+                throw new ProblemException("Invalid token");
+            }
+        } finally {
+            loggedUsersLock.writeLock().unlock();
+        }
+    }
+
+    public Set<Id> listNoticesIds(UUID token) throws ProblemException {
+        if (isValid(token)) {
+            noticesLock.readLock().lock();
+            try {
+                return ImmutableSet.copyOf(notices.keySet());
+            } finally {
+                noticesLock.readLock().unlock();
+            }
+        } else {
+            throw new ProblemException("Invalid token");
+        }
+    }
+
+    @Override
+    public Id addNotice(UUID token, String title, String message) throws ProblemException {
+        if (isValid(token)) {
+            Id id = new TitleId(title);
+            noticesLock.writeLock().lock();
+            try {
+                if (notices.get(id) == null) {
+                    notices.put(id, new Notice(title, message));
+                    return id;
+                } else {
+                    throw new ProblemException("Topic with title '" + title + "' already exists");
+                }
+            } finally {
+                noticesLock.writeLock().unlock();
+            }
+        } else {
+            throw new ProblemException("Invalid token");
+        }
+    }
+
+    @Override
+    public Notice getNotice(UUID token, Id id) throws ProblemException {
+        if (isValid(token)) {
+            Notice notice;
+            noticesLock.readLock().lock();
+            try {
+                notice = notices.get(id);
+            } finally {
+                noticesLock.readLock().unlock();
+            }
+            if (notice == null) {
+                throw new ProblemException("There is no such notice '" + id + "'");
+            } else {
+                return notice;
+            }
+        } else {
+            throw new ProblemException("Invalid token");
+        }
+    }
+
+    @Override
+    public Id updateNotice(UUID token, Id id, String title, String message) throws ProblemException {
+        if (isValid(token)) {
+            noticesLock.writeLock().lock();
+            try {
+                final Notice notice = notices.get(id);
+                if (notice == null) {
+                    throw new ProblemException("There is no such notice '" + id + "'");
+                } else {
+                    Id newId = new TitleId(title);
+                    if (!newId.equals(id) && notices.containsKey(newId)) {
+                        throw new ProblemException("Topic with title '" + title + "' already exists");
+                    } else {
+                        if (!newId.equals(id)) {
+                            notices.remove(id);
+                        }
+                        notices.put(newId, new Notice(title, message));
+                        return newId;
+                    }
+                }
+            } finally {
+                noticesLock.writeLock().unlock();
+            }
+        } else {
+            throw new ProblemException("Invalid token");
+        }
+    }
+
+    @Override
+    public void deleteNotice(UUID token, Id id) throws ProblemException {
+        if (isValid(token)) {
+            Notice notice;
+            noticesLock.writeLock().lock();
+            try {
+                notice = notices.remove(id);
+            } finally {
+                noticesLock.writeLock().unlock();
+            }
+            if (notice == null) {
+                throw new ProblemException("There is no such notice '" + id + "'");
+            }
+        } else {
+            throw new ProblemException("Invalid token");
+        }
+    }
+}
--- src/test/java/edu/agh/lroza/locks/CustomLocksServerJavaTest.scala
+++ src/test/java/edu/agh/lroza/locks/CustomLocksServerJavaTest.scala
@@ -1,14 +1,15 @@
-//package edu.agh.lroza.locks
-//
-//import org.scalatest.FunSuite
-//import edu.agh.lroza.common.FunSuiteServerBeahaviors
-//import org.junit.runner.RunWith
-//import org.scalatest.junit.JUnitRunner
-//
-//@RunWith(classOf[JUnitRunner])
-//class CustomLocksServerJavaTest extends FunSuite with FunSuiteServerBeahaviors {
-//  def server = new CustomLocksServerJava();
-//
-//  basicLogInLogOut(server)
-//  noticesManagement(server)
-//}
+package edu.agh.lroza.locks
+
+import org.scalatest.FunSuite
+import edu.agh.lroza.common.FunSuiteServerBeahaviors
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import edu.agh.lroza.NoticeBoardServerJavaWrapper
+
+@RunWith(classOf[JUnitRunner])
+class CustomLocksServerJavaTest extends FunSuite with FunSuiteServerBeahaviors {
+  def server = new NoticeBoardServerJavaWrapper(new CustomLocksServerJava());
+
+  basicLogInLogOut(server)
+  noticesManagement(server)
+}
